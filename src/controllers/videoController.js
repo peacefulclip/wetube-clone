53b72eb5cbe860f16b5adff1fe912869f3d08187
peacefulclip <github-email>
2024-07-13 23:19:39 +0900

    #5.9 Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,24 @@
 
 
 export const trending = (req, res) => {
-    const videos = []
+    const videos = [
+        {
+            title: "first video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1
+        },
+        {
+            title: "second video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1
+        }
+    ]
     res.render("home", {pageTitle: "Home", videos})
 }
 export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});

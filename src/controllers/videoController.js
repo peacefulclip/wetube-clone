df213f052c2a51a970ab21d7b1c2b63ebf5111c2
peacefulclip <github-email>
2024-07-14 21:01:13 +0900

    #6.0 Array Database part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,27 +1,38 @@
-
+const videos = [
+    {
+        title: "first video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "second video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 2,
+    },
+    {
+        title: "third video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 3,
+    }
+];
 
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "first video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1
-        },
-        {
-            title: "second video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1
-        }
-    ]
     res.render("home", {pageTitle: "Home", videos})
 }
-export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
 export const deleteVideo = (req, res) => res.send("delete");
